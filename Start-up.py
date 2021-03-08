79ec1ac164345aef3853fd03bc2191c2015a3376
CrowntailTW0608 <72938929+CrowntailTW0608@users.noreply.github.com>
2021-03-08 17:27:56 +0800

    Create Start-up.py

---
+++ Start-up.py
@@ -0,0 +1,17 @@
+import os
+import re
+import requests
+from bs4 import BeautifulSoup
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import warnings
+import cloudscraper
+
+
+
+def fxn():
+    warnings.warn("deprecated", DeprecationWarning)
+with warnings.catch_warnings():
+    warnings.simplefilter("ignore")
+    fxn()
