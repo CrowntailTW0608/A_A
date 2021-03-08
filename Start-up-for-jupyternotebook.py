7dbbac64edf1a5b970c4a99412a75efabd037989
CrowntailTW0608 <72938929+CrowntailTW0608@users.noreply.github.com>
2021-03-08 17:28:58 +0800

    Create Start-up-for-jupyternotebook.py

---
+++ Start-up-for-jupyternotebook.py
@@ -0,0 +1,29 @@
+class display(object):
+    """Display HTML representation of multiple objects"""
+    template = """<div style="float: left; padding: 10px;">
+    <p style='font-family:"Courier New", Courier, monospace'>{0}</p>{1}
+    </div>"""
+    def __init__(self, *args):
+        self.args = args
+        
+    def _repr_html_(self):
+        return '\n'.join(self.template.format(a, eval(a)._repr_html_())
+                         for a in self.args)
+    
+    def __repr__(self):
+        return '\n\n'.join(a + '\n' + repr(eval(a))
+                           for a in self.args)
+      
+      
+
+d = {'col1': [1, 2], 'col2': [3, 4]}
+df = pd.DataFrame(data=d)
+
+df2 = pd.DataFrame(np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]]),columns=['a', 'b', 'c'])
+
+display('df', 'df2')
+      
+      
+      
+      
+      
